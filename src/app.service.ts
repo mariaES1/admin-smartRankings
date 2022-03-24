18f2d7b05fe66d7546fd0f09d0e65cc0f8fe6b39
Maria Eduarda <mariaeduarda-souza@hotmail.com.br>
2022-03-24 14:53:15 -0300

    feat: add service to create a category

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Categories } from './interfaces/categories/category.interface';
+import { Player } from './interfaces/players/player.interface';
+import { Model } from 'mongoose';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('Category') private readonly categoryModel: Model<Categories>,
+    @InjectModel('Player') private readonly playerModel: Model<Player>,
+  ) {}
+
+  private readonly logger = new Logger(AppService.name);
+
+  async createCategory(category: Categories): Promise<Categories> {
+    try {
+      const createdCategory = new this.categoryModel(category);
+      return await createdCategory.save();
+    } catch (error) {
+      this.logger.log(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
   }
 }
