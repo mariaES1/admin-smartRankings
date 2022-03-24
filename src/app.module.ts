2714273d5281301c47506e361a5117a74d44e4ec
Maria Eduarda <mariaeduarda-souza@hotmail.com.br>
2022-03-24 14:43:48 -0300

    refactor: import schemas onto module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesSchema } from './interfaces/categories/category.schema';
+import { PlayerSchema } from './interfaces/players/player.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Maria:DlkcN4qR7VhlH8Sw@cluster0.uvx5v.mongodb.net/srAdmin?retryWrites=true&w=majority',
+    ),
+    MongooseModule.forFeature([
+      { name: 'Cetegory', schema: CategoriesSchema },
+      { name: 'Player', schema: PlayerSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/interfaces/players/player.schema.ts
+++ src/interfaces/players/player.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-export const playerSchema = new mongoose.Schema(
+export const PlayerSchema = new mongoose.Schema(
   {
     email: { type: String, unique: true },
     phoneNumber: { type: String },
